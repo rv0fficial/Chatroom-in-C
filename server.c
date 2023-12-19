3344c177b0b83bc4f3cffaa2b288abf0f97f7523
Rashmika Viduranga <147927710+rv0fficial@users.noreply.github.com>
2023-12-19 12:58:44 +0530

    Update server.c

--- server.c
+++ server.c
@@ -10,11 +10,11 @@
 #include <sys/types.h>
 #include <signal.h>
 
-#define MAX_CLIENTS 100
+#define MAX_CLIENTS 4
 #define BUFFER_SZ 2048
 
-static _Atomic unsigned int cli_count = 0;
-static int uid = 10;
+static int cli_count = 0;
+static int uid = 4;
 
 /* Client structure */
 typedef struct{
@@ -55,7 +55,8 @@ void print_client_addr(struct sockaddr_in addr){
 void queue_add(client_t *cl){
 	pthread_mutex_lock(&clients_mutex);
 
-	for(int i=0; i < MAX_CLIENTS; ++i){
+int i;
+	for(i=0; i < MAX_CLIENTS; ++i){
 		if(!clients[i]){
 			clients[i] = cl;
 			break;
@@ -68,8 +69,8 @@ void queue_add(client_t *cl){
 /* Remove clients to queue */
 void queue_remove(int uid){
 	pthread_mutex_lock(&clients_mutex);
-
-	for(int i=0; i < MAX_CLIENTS; ++i){
+int i;
+	for(i=0; i < MAX_CLIENTS; ++i){
 		if(clients[i]){
 			if(clients[i]->uid == uid){
 				clients[i] = NULL;
@@ -84,12 +85,12 @@ void queue_remove(int uid){
 /* Send message to all clients except sender */
 void send_message(char *s, int uid){
 	pthread_mutex_lock(&clients_mutex);
-
-	for(int i=0; i<MAX_CLIENTS; ++i){
+int i;
+	for(i=0; i<MAX_CLIENTS; ++i){
 		if(clients[i]){
 			if(clients[i]->uid != uid){
 				if(write(clients[i]->sockfd, s, strlen(s)) < 0){
-					perror("ERROR: write to descriptor failed");
+					printf("ERROR: write to descriptor failed");
 					break;
 				}
 			}
@@ -157,19 +158,15 @@ void *handle_client(void *arg){
 	return NULL;
 }
 
-int main(int argc, char **argv){
-	if(argc != 2){
-		printf("Usage: %s <port>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+int main(int argc){
 
-	char *ip = "127.0.0.1";
-	int port = atoi(argv[1]);
-	int option = 1;
-	int listenfd = 0, connfd = 0;
-  struct sockaddr_in serv_addr;
-  struct sockaddr_in cli_addr;
-  pthread_t tid;
+	char 		*ip = "127.0.0.1";
+	int 		port = 66666;
+	int 		option = 1;
+	int 		listenfd = 0, connfd = 0;
+  	struct 		sockaddr_in serv_addr;
+  	struct 		sockaddr_in cli_addr;
+  	pthread_t 	tid;
 
   /* Socket settings */
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -181,29 +178,29 @@ int main(int argc, char **argv){
 	signal(SIGPIPE, SIG_IGN);
 
 	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
-		perror("ERROR: setsockopt failed");
+		printf("ERROR: setsockopt failed");
     return EXIT_FAILURE;
 	}
 
 	/* Bind */
   if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("ERROR: Socket binding failed");
+    printf("ERROR: Socket binding failed");
     return EXIT_FAILURE;
   }
 
   /* Listen */
   if (listen(listenfd, 10) < 0) {
-    perror("ERROR: Socket listening failed");
+    printf("ERROR: Socket listening failed");
     return EXIT_FAILURE;
 	}
 
-	printf("=== WELCOME TO THE CHATROOM ===\n");
+	printf("-----\xE2\x99\xA0 WELCOME TO THE CHAT SERVER \xE2\x99\xA0----\n");
 
 	while(1){
 		socklen_t clilen = sizeof(cli_addr);
 		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
 
-		/* Check if max clients is reached */
+		// Check if max clients is reached
 		if((cli_count + 1) == MAX_CLIENTS){
 			printf("Max clients reached. Rejected: ");
 			print_client_addr(cli_addr);
@@ -212,17 +209,17 @@ int main(int argc, char **argv){
 			continue;
 		}
 
-		/* Client settings */
+		// Client settings
 		client_t *cli = (client_t *)malloc(sizeof(client_t));
 		cli->address = cli_addr;
 		cli->sockfd = connfd;
 		cli->uid = uid++;
 
-		/* Add client to the queue and fork thread */
+		// Add client to the queue and fork thread
 		queue_add(cli);
 		pthread_create(&tid, NULL, &handle_client, (void*)cli);
 
-		/* Reduce CPU usage */
+		// Reduce CPU usage
 		sleep(1);
 	}
 
